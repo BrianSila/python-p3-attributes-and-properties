f93aecee0fb9b289a663e05841713cf36a19ddd0
Brian <brian.sila1@student.moringaschool.com>
2025-05-09 13:20:41 +0300

    attributes anad properties hacked

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name: str = "Brian", breed: str = "Shar Pei"):
+        self.name = name  
+        self.breed = breed 
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @name.setter
+    def name(self, new_name: str):
+        if not isinstance(new_name, str) or not (1 <= len(new_name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = new_name
+
+    @property
+    def breed(self) -> str:
+        return self._breed
+    
+    @breed.setter
+    def breed(self, new_breed: str):
+        if new_breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = new_breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name: str = "Unknown", job: str = "Admin"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, new_name: str):
+        if not isinstance(new_name, str) or not (1 <= len(new_name.strip()) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = new_name.strip().title()
+
+    @property
+    def job(self) -> str:
+        return self._job
+    
+    @job.setter
+    def job(self, new_job: str):
+        if new_job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = new_job
